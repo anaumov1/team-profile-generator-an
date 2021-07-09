a816db76497ced489a81dedcafa8baf6fa12b060
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-08 23:44:31 -0500

    failing test for Intern

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,23 +2,23 @@ const Engineer = require("../lib/Engineer");
 
 // extends employee
 test('creates a Employee object', () => {
-    const engineer = new Engineer('john');
+    const engineer = new Engineer('Alex');
     
-    expect(engineer.name).toBe('john');
+    expect(engineer.name).toBe('Alex');
     expect(engineer.id).toBe(this.id);
     expect(engineer.email).toBe(this.email);
 });
 
 // getGithub()
 test('tests to make sure the engineers github has been added', () => {
-    const engineer = new Engineer('john');
+    const engineer = new Engineer('Alex');
 
     expect(engineer.getGithub()).toBe(this.github);
 });
 
 // getRole() // Overridden to return Engineer
 test('tests to make sure the engineers role has been overriden from employee to engineer', () => {
-    const engineer = new Engineer('john');
+    const engineer = new Engineer('Alex');
 
     expect(engineer.getRole()).toMatch('Engineer');
 });
